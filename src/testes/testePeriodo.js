046f578bae547698b29c073c117ba61c517d7336
Airtonrgj <jarodriguesj1@gmail.com>
2026-04-24 10:26:15 -0300

    feat(domain): adiciornar export feat(testes): confirgurar testes para leitura de checkin e checkout feat(json): configurar type do jason para  receber em módulos

---
+++ src/testes/testePeriodo.js
@@ -0,0 +1,24 @@
+import PeriodoStadia from "../domain/valueObjects/Periodo.js";
+function testarPeriodo(){
+    console.log("iniciando o teste")
+
+    /* primeiro teste: período válido */
+    console.log("teste 1...")
+    try{
+        const periodo = new PeriodoStadia(
+            "2026-05-10",
+            "2026-05-15"
+        )
+        console.log("cadastro criado com sucesso")
+        console.log(" checkin: ", periodo.checkin.toLocaleDateString())
+        console.log(" checkout: ", periodo.checkout.toLocaleDateString())
+    } catch(erro){
+        console.log("o periodo do checkin não pode ser uma data superior a do checkout")
+    
+    }
+
+    
+
+}
+
+testarPeriodo()
