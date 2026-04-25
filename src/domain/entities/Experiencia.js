bc3cf31cd01c9bbe098bf8567aa58eb3cb6da781
Airtonrgj <jarodriguesj1@gmail.com>
2026-04-25 16:57:29 -0300

    configurei alguns métodos para Reserva.js e criei o construtor para o campo de Esperiencia.js

---
+++ src/domain/entities/Experiencia.js
@@ -0,0 +1,10 @@
+class experiencia{
+    constructor(id, nome, tempo, dificuldade, horario, vestuario_indicado){
+        this.id = id
+        this.nome = nome
+        this.dificuldade = dificuldade
+        this.horario = horario
+        this.vestuario_indicado = vestuario_indicado
+
+    }
+}
