6bfe1436a8c348cbdf903d8d6aa4f3d4ebc3fc9b
limademarina <marina.fonseca@somosicev.com>
2026-04-27 15:58:43 -0300

    Lógica do experienciaStorage.

--- src/infrastructure/storage/experienciaStorage.js
+++ src/infrastructure/storage/experienciaStorage.js
@@ -1,10 +1,38 @@
-listarExperiencias()
+const = CHAVE = 'experiencias'
+
+function listarExperiencias(){
+    const texto = localStorage.getItem(CHAVE)
+    if (!texto) return []
+    return JSON.parse(texto)
+}
+
+function salvarExperiencia(experiencia){
+    const lista = listarExperiencias()
+    lista.push(experiencia)
+    const texto = JSON.stringify(lista)
+    localStorage.setItem(CHAVE, texto)
+}
+
+function buscarExperienciaPorId(id){
+    const lista = listarExperiencias() //pega todas as experiências
+    return lista.find(e => e.id === id)  //procura e devolve
+}
 
 
-salvarExperiencia(experiencia)
+function atualizarExperiencia(experienciaAtualizada){
+    const lista = listarExperiencias() // pega a lista existente
+    const novaLista = lista.map(e => {              
+    if (e.id === experienciaAtualizada.id) // se achar
+      return experienciaAtualizada // retorna a versão nova
+    return e //caso não ache, retorna a atual
+  })
+  localStorage.setItem(CHAVE, JSON.stringify(novaLista)) // salva tudo
+}
 
-buscarExperienciaPorId()
+function deletarExperiencia(id){
+    const lista = listarExperiencias()                 // pega a lista atual
+      const novaLista = lista.filter(e => e.id !== id) // remove a do id
+      localStorage.setItem(CHAVE, JSON.stringify(novaLista)) // salva sem ela
+}
 
-atualizarExperiencia(experienciaAtualizada)
-
-deletarExperiencia(id)
+export { listarExperiencias, salvarExperiencia, buscarExperienciaPorId, atualizarExperiencia, deletarExperiencia }
